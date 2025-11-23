bed4062ea5c828c2d1d406b1b0e5099855fb31f5
ivanpro592 <daniladaskov473@gmail.com>
2025-11-23 13:52:19 +0400

    added filteredTodos and some logic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 let id = 0
 
 const newTodo = ref('')
-
 const todos = ref([])
+const hideCompleted = ref(false)
+
 const addTodo = () => {
 	if (newTodo.value.trim() === '') return
 	todos.value.push({
@@ -17,28 +18,39 @@ const addTodo = () => {
 const removeTodo = todo => {
 	todos.value = todos.value.filter(t => t != todo)
 }
+const filteredTodos = computed(() => {
+	return hideCompleted.value ? todos.value.filter(t => !t.done) : todos.value
+})
 </script>
 
 <template>
 	<div class="container">
 		<h1 class="title">TODO APP</h1>
-		<p class="subtitle">Количество задач:</p>
+		<p class="subtitle">Количество задач: {{}}</p>
 		<div class="body">
 			<form @submit.prevent="addTodo">
 				<input v-model="newTodo" required />
 				<button class="btn">Добавить</button>
 			</form>
 			<ul class="item-list">
-				<li class="item" v-for="todo in todos" :key="todo.id">
-					{{ todo.text }}
+				<li class="item" v-for="todo in filteredTodos" :key="todo.id">
+					<input type="checkbox" class="input__checkbox" v-model="todo.done" />
+					<span :class="{ done: todo.done }">{{ todo.text }}</span>
 					<button class="btn__delete" @click="removeTodo(todo)">X</button>
 				</li>
 			</ul>
+			<button @click="hideCompleted = !hideCompleted">
+				{{ hideCompleted ? 'Показать все' : 'Скрыть выполненные' }}
+			</button>
 		</div>
 	</div>
 </template>
 
 <style scoped>
+.done {
+	text-decoration: line-through;
+}
+
 .container {
 	display: flex;
 	flex-direction: column;
@@ -55,5 +67,10 @@ const removeTodo = todo => {
 .btn__delete {
 	width: 20px;
 	height: 30px;
+}
+
+.input__checkbox {
+	width: 20px;
+	height: 20px;
 }
 </style>
